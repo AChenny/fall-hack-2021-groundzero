2fde78820503e6e4c8cbc64a1fe26f1de886a0db
gpark608 <46937752+gpark608@users.noreply.github.com>
2021-11-27 15:00:44 -0800

    Co-authored-by: Spencer Leung <SpencerLeung1024@users.noreply.github.com> Co-authored-by: Sanseerat Virk <ssv3@sfu.ca> Co-authored-by: AChenny <AChenny@users.noreply.github.com>

--- app/views.py
+++ app/views.py
@@ -8,4 +8,16 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanen
 class IndexView(generic.DetailView):
     INDEX = 'index.html'
     def get(self, request, *args, **kwargs):
-        return render(request, self.INDEX)
+        # fig = gmaps.figure()
+        # # locations = [(49.23757, -122.99703), (49.22087, -123.00913)]
+        # # locations = [(46.1, 5.2), (46.2, 5.3), (46.3, 5.4)]
+        # # fig.add_layer(gmaps.heatmap_layer(locations))
+        # # fig = gmaps.figure(center=(49, -122.0), zoom_level=8)
+
+        # locations = [(46.1, 5.2), (46.2, 5.3), (46.3, 5.4)]
+        # heatmap = gmaps.heatmap_layer(locations)
+
+        # embed_minimal_html('export.html', views=[fig])
+
+
+        return render(request, self.INDEX, {"hello": "Hello"})
---
+++ test.py
@@ -0,0 +1,26 @@
+import gmaps
+from ipywidgets.embed import embed_minimal_html
+
+gmaps.configure(api_key="AIzaSyBCezsABWlWkGzswTzlrnjiW24MfJE1BTQ")
+
+fig = gmaps.figure()
+# locations = [(49.23757, -122.99703), (49.22087, -123.00913)]
+# locations = [(46.1, 5.2), (46.2, 5.3), (46.3, 5.4)]
+# fig.add_layer(gmaps.heatmap_layer(locations))
+# fig = gmaps.figure(center=(49, -122.0), zoom_level=8)
+
+locations = [(46.1, 5.2), (46.2, 5.3), (46.3, 5.4)]
+weights = [0.5, 0.2, 0.8]
+heatmap = gmaps.heatmap_layer(locations, weights=weights)
+heatmap.max_intensity = 3.5
+heatmap.weights = [3, 3, 3]
+fig.add_layer(heatmap)
+
+# heatmap = gmaps.heatmap_layer(locations)
+# heatmap.max_intensity = 2
+# heatmap.point_radius = 3
+# heatmap.gradient = ['white', 'gray']
+# fig.add_layer(heatmap)
+
+embed_minimal_html('export.html', views=[fig])
+
